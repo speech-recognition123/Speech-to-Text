a67dc2666493e414bda9d9872009e2b03f314291
Abel Mitiku <abelmgetnet@gmail.com>
2022-05-31 12:56:42 +0300

    added css sheets path

--- app.py
+++ app.py
@@ -15,3 +15,13 @@ app = MultiApp()
 st.sidebar.markdown("""
 # Speech to Text
 """)
+
+with open('./scripts/web-css/styles.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+# Add all your application here
+app.add_app("visualizations", visualizations.app)
+app.add_app("model-prediction", viz_model.app)
+
+# The main app
+app.run()
