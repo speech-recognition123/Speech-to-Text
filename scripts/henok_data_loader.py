3f9dcab1fa099dd436819fc40f846963a8d32c2d
Hen0k <henoktilaye7@gmail.com>
2022-06-01 07:56:47 +0300

    impl: add a transcription and audio filename pair loader

---
+++ scripts/henok_data_loader.py
@@ -0,0 +1,28 @@
+import librosa
+import os
+import sys
+from log_helper import App_Logger
+sys.path.insert(0, '../logs/')
+sys.path.append(os.path.abspath(os.path.join('..')))
+
+
+class DataLoader:
+
+    def __init__(self) -> None:
+        my_logger = App_Logger("../logs/henok_dat_loader.log")
+        self.logger = my_logger.get_app_logger()
+
+    def transcription_loader(self, filename):
+        name_to_text = {}
+        with open (filename, encoding="utf-8") as f:
+            for line in f:
+                name=line.split("</s>")[1]
+                name=name.replace('(', '')
+                name=name.replace(')', '')
+                name=name.replace('\n','')
+                name=name.replace(' ','')
+                text=line.split("</s>")[0]
+                text=text.replace("<s>","")
+                name_to_text[name]=text
+            self.logger(f"audio-transcription pairs generated")
+            return name_to_text
