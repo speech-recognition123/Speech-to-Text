c1a0421b375f4976ad5c4e7bcea9a89080dc8eb5
Hen0k <henoktilaye7@gmail.com>
2022-06-01 07:58:13 +0300

    impl: add a cleaning module with audio sample resizing method

---
+++ scripts/henok_audio_cleaning.py
@@ -0,0 +1,25 @@
+import numpy as np
+import librosa
+import os
+import sys
+from log_helper import App_Logger
+sys.path.insert(0, '../logs/')
+sys.path.append(os.path.abspath(os.path.join('..')))
+
+
+class AudioCleaner:
+
+    def __init__(self) -> None:
+        my_logger = App_Logger("../logs/henok_audio_cleaner.log")
+        self.logger = my_logger.get_app_logger()
+
+    def resize_audio(self, audio: np.Array, size: int) -> np.Array:
+        """
+        This resizes all input audio to a fixed sample size.
+        It helps us to have a consistent data shape
+        """
+        resized = librosa.utils.fix_length(audio, size, axis=1)
+        self.logger.info(f"Audio resized to {size} samples")
+        return resized
+
+    # def convert_to_sterio(self, audio: np.Array) -> np.Array:
