c7409a14db05364034976a1aa5b28e0625638870
Hen0k <henoktilaye7@gmail.com>
2022-06-01 07:57:33 +0300

    impl: add audio metadata generator. For not it only calculates duration

--- scripts/henok_data_loader.py
+++ scripts/henok_data_loader.py
@@ -14,15 +14,30 @@ class DataLoader:
 
     def transcription_loader(self, filename):
         name_to_text = {}
-        with open (filename, encoding="utf-8") as f:
+        with open(filename, encoding="utf-8") as f:
             for line in f:
-                name=line.split("</s>")[1]
-                name=name.replace('(', '')
-                name=name.replace(')', '')
-                name=name.replace('\n','')
-                name=name.replace(' ','')
-                text=line.split("</s>")[0]
-                text=text.replace("<s>","")
-                name_to_text[name]=text
+                name = line.split("</s>")[1]
+                name = name.replace('(', '')
+                name = name.replace(')', '')
+                name = name.replace('\n', '')
+                name = name.replace(' ', '')
+                text = line.split("</s>")[0]
+                text = text.replace("<s>", "")
+                name_to_text[name] = text
             self.logger(f"audio-transcription pairs generated")
             return name_to_text
+
+    def audio_metadata_generator(self, transcription, path):
+        target = []
+        features = []
+        filenames = []
+        duration_of_recordings = []
+        for key in transcription:
+            filename = path + key + ".wav"
+            filenames.append(filename)
+            audio, fs = librosa.load(filename, sr=None)
+            duration_of_recordings.append(librosa.get_duration(y=audio, sr=fs))
+            label = transcription[key]
+            target.append(label)
+        self.logger(f"audio-metadata generated")
+        return filenames, target, duration_of_recordings
