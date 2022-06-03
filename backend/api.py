ea7c2f452a84e7bc7ce58358f43924477cd384cd
Nahom Bekele <nahom.fix@gmail.com>
2022-06-03 18:04:19 +0300

    feat: add prediction endpoint

---
+++ backend/api.py
@@ -0,0 +1,34 @@
+import os
+from datetime import datetime
+from pickle import load
+
+from flask import Flask, jsonify, make_response, request
+
+app = Flask(__name__)
+
+
+@app.route("/health", methods=["GET"])
+def health():
+    return make_response(
+        jsonify(
+            {
+                "success": True,
+                "timestamp": datetime.now().isoformat(),
+            }
+        ),
+        200,
+    )
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    file = request.files["file"]
+    pwd = os.getcwd()
+    rnn_model_path = os.path.join(pwd, "model/RNN_model.pickle")
+    rnn_model = load(open(rnn_model_path, "rb"))
+    prediction = rnn_model.predict(file)
+    return make_response(jsonify({"success": True, "data": prediction}), 200)
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=5000, debug=True)
