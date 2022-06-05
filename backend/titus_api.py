66014f5716d0bd90c965d52a31d6377a7f416b88
waCira <wachura11t@gmail.com>
2022-06-05 23:03:38 +0300

    Add prediction file

--- backend/titus_api.py
+++ backend/titus_api.py
@@ -11,6 +11,17 @@ import os
 #Initialize App
 app = Flask(__name__)
 
+#Route to handle the prediction
+@app.route("/predict", methods=['POST'])
+def predict():
+    file = request.files["file"]
+    pwd = os.getcwd()
+    rnn_model_path = os.path.join(pwd, "model/RNN_model.pickle")
+    rnn_model = load(open(rnn_model_path, "rb"))
+    prediction = rnn_model.predict(file)
+    return make_response(jsonify({"success": True, "data": prediction}), 200)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
