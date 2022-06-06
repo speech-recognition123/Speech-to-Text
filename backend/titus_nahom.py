13380c0bd6228b6c8c916035483e96eacb4a2347
waCira <wachura11t@gmail.com>
2022-06-06 08:37:39 +0300

    Create dictionary to be served by model

---
+++ backend/titus_nahom.py
@@ -0,0 +1,43 @@
+import os
+from datetime import datetime
+from pickle import load
+
+from flask import Flask, jsonify, make_response, request
+
+app = Flask(__name__)
+
+
+@app.route("/health", methods=["GET"])
+def health():
+    return make_response(
+        jsonify(
+            {
+                "success": True,
+                "timestamp": datetime.now().isoformat(),
+            }
+        ),
+        200,
+    )
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    path = request.json['path']
+    text = request.json['text']
+    duration = request.json['duration']
+
+    #Assemble files to form a new dictionary
+    file = {"path": path,
+            "text": text,
+            "duration": duration}
+
+    file = request.files["file"]
+    pwd = os.getcwd()
+    rnn_model_path = os.path.join(pwd, "model/RNN_model.pickle")
+    rnn_model = load(open(rnn_model_path, "rb"))
+    prediction = rnn_model.predict(file)
+    return make_response(jsonify({"success": True, "data": prediction}), 200)
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=5000, debug=True)
