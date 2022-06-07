8aaaf1195cd8156a3b2a03057b723fc2b97c7f98
Samrawit02 <hairaer02.gmail.com>
2022-06-07 14:23:10 +0300

    modifiying app js

--- web/frontend/src/App.js
+++ web/frontend/src/App.js
@@ -1,21 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React , { useState } from "react";
+import { Link } from "react-router-dom";
+
+import axios from "axios";
+
+
+let gumStream = null;
+let recorder = null;
+let audioContext = null;
 
 function App() {
-  return (
-    <div className="app">
-       
-          <p className='p-mic'>Microphone: {Listening ? "NO" :"OFF"}</p>
-            <div className='button'>
-            <button onClick={startRecording} type="button">Start</button>
-            <button onClick={stopRecording} type="button">Stop</button>
-        </div>
-        <div className="p-transcription"> 
-        Text desplayed here
-        </div>
-        </div>
-  
-  );
+  const startRecording = () => {
+  let constraints = {
+    audio: true,
+    video: false
 }
+
+audioContext = new window.AudioContext();
+console.log("sample rate: " + audioContext.sampleRate);
+
+navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(function (stream) {
+        console.log("initializing Recorder.js ...");
+
+        gumStream = stream;
+
+        let input = audioContext.createMediaStreamSource(stream);
+
+        recorder = new window.Recorder(input, {
+            numChannels: 1
+        })
+
+        recorder.record();
+        console.log("Recording started");
+    }).catch(function (err) {
+        //enable the record button if getUserMedia() fails
+});
+
+}
+
+
+const stopRecording = () => {
+console.log("stopButton clicked");
+
+recorder.stop(); //stop microphone access
+gumStream.getAudioTracks()[0].stop();
+
+recorder.exportWAV(onStop);
+}
+
+const onStop = (blob) => {
+console.log("uploading...");
+
+let data = new FormData();
+
+data.append('text', "this is the transcription of the audio file");
+data.append('wavfile', blob, "recording.wav");
+
+const config = {
+    headers: {'content-type': 'multipart/form-data'}
+}
+axios.post('http://localhost:8080/asr/', data, config);
+}
+
+return (
+    <>
+<div className ='app'>
+    <button onClick={startRecording} type="button">Start</button>
+    <button onClick={stopRecording} type="button">Stop</button>
+</div>
+  
+    </>
+  );
+};
+
 
 export default App;
