f4a681aba47a4e951fdf2d1f6159b6d139d6e8b8
Samrawit02 <hairaer02.gmail.com>
2022-06-08 11:21:41 +0300

    updating the audio record file

--- web/frontend/src/App.js
+++ web/frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { useState }  from 'react'
 import AudioUpload from './AudioUpload'
+import AudioFileUploader from "./components/AudioRecorder";
 import axios from 'axios';
 
 import "./App.css";
@@ -13,71 +14,72 @@ let audioContext = null;
 
 
 function App() {
-  const startRecording = () => {
-  let constraints = {
-    audio: true,
-    video: false
-}
+//   const startRecording = () => {
+//   let constraints = {
+//     audio: true,
+//     video: false
+// }
 
-audioContext = new window.AudioContext();
-console.log("sample rate: " + audioContext.sampleRate);
+// audioContext = new window.AudioContext();
+// console.log("sample rate: " + audioContext.sampleRate);
 
-navigator.mediaDevices
-    .getUserMedia(constraints)
-    .then(function (stream) {
-        console.log("initializing Recorder.js ...");
+// navigator.mediaDevices
+//     .getUserMedia(constraints)
+//     .then(function (stream) {
+//         console.log("initializing Recorder.js ...");
 
-        gumStream = stream;
+//         gumStream = stream;
 
-        let input = audioContext.createMediaStreamSource(stream);
+//         let input = audioContext.createMediaStreamSource(stream);
 
-        recorder = new window.Recorder(input, {
-            numChannels: 1
-        })
+//         recorder = new window.Recorder(input, {
+//             numChannels: 1
+//         })
 
-        recorder.record();
-        console.log("Recording started");
-    }).catch(function (err) {
-        //enable the record button if getUserMedia() fails
-});
+//         recorder.record();
+//         console.log("Recording started");
+//     }).catch(function (err) {
+//         //enable the record button if getUserMedia() fails
+// });
 
-}
+// }
 
 
-const stopRecording = () => {
-console.log("stopButton clicked");
+// const stopRecording = () => {
+// console.log("stopButton clicked");
 
-recorder.stop(); //stop microphone access
-gumStream.getAudioTracks()[0].stop();
+// recorder.stop(); //stop microphone access
+// gumStream.getAudioTracks()[0].stop();
 
-recorder.exportWAV(onStop);
-}
+// recorder.exportWAV(onStop);
+// }
 
-const onStop = (blob) => {
-console.log("uploading...");
+// const onStop = (blob) => {
+// console.log("uploading...");
 
-let data = new FormData();
+// let data = new FormData();
 
-data.append('text', "this is the transcription of the audio file");
-data.append('wavfile', blob, "recording.wav");
+// data.append('text', "this is the transcription of the audio file");
+// data.append('wavfile', blob, "recording.wav");
 
-const config = {
-    headers: {'content-type': 'multipart/form-data'}
-}
-axios.post('http://localhost:8080/asr/', data, config);
-}
+// const config = {
+//     headers: {'content-type': 'multipart/form-data'}
+// }
+// axios.post('http://localhost:8080/asr/', data, config);
+// }
 
 return (
 
 <>
     <div className="App">
       <AudioUpload />
-    </div>
+      <AudioFileUploader/>
+    {/* </div>
 
       <div className='app'>
         <button onClick={startRecording} type="button">Start</button>
         <button onClick={stopRecording} type="button">Stop</button>
-      </div>
+      </div> */}
 
     </>
   );
---
+++ web/frontend/src/components/AudioRecorder.js
@@ -0,0 +1,114 @@
+
+import axios from "axios";
+
+import React, { useState } from "react";
+import "./AudioRecorder.css";
+
+
+let gumStream = null;
+let recorder = null;
+let audioContext = null;
+
+const AudioRecorder = () => {
+  // const [show, setShow] = useState(false);
+  // const [view, setView] = useState(true);
+
+  const startRecording = () => {
+    let constraints = {
+      audio: true,
+      video: false,
+    };
+
+    audioContext = new window.AudioContext();
+    console.log("sample rate: " + audioContext.sampleRate);
+
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then(function (stream) {
+        console.log("initializing Recorder.js ...");
+
+        gumStream = stream;
+
+        let input = audioContext.createMediaStreamSource(stream);
+        console.log(input);
+
+        recorder = new window.Recorder(input, {
+          numChannels: 1,
+        });
+
+        console.log(recorder);
+        recorder.record();
+        console.log("Recording started");
+      })
+      .catch(function (err) {
+        //enable the record button if getUserMedia() fails
+        console.log(err);
+      });
+  };
+
+  const stopRecording = () => {
+    console.log("stopButton clicked");
+
+    recorder.stop(); //stop microphone access
+    gumStream.getAudioTracks()[0].stop();
+
+    recorder.exportWAV(onStop);
+  };
+
+  const onStop = (blob) => {
+    console.log("uploading...");
+
+    let data = new FormData();
+
+    data.append("text", "this is the transcription of the audio file");
+    data.append("wavfile", blob, "recording.wav");
+
+    const config = {
+      headers: { "content-type": "multipart/form-data" },
+    };
+    console.log(data);
+    axios.post("http://localhost:8080/asr/", data, config);
+  };
+
+  // const [file, setFile] = useState();
+
+  // function handleChange(event) {
+  //   setFile(event.target.files[0]);
+  // }
+
+  // function handleSubmit(event) {
+  //   event.preventDefault();
+  //   //const url = "http://localhost:3000/uploadFile";
+  //   const formData = new FormData();
+  //   formData.append("file", file);
+  //   formData.append("fileName", file.name);
+  //   console.log(file.name);
+
+  // }
+
+  return (
+ <>
+      <div className='container'>
+
+            <div className='display'>
+              <div>
+                <div>
+                  <h2>Record Audio</h2>
+                  <button onClick={startRecording} type="button">
+                    Start
+                  </button>
+                  <button onClick={stopRecording} type="button">
+                    Stop
+                  </button>
+                </div>
+                <br></br>
+                <br></br>
+                <audio autoPlay controls></audio>
+              </div>
+              
+            </div>
+      </div>
+       </>
+  )
+};
+export  default AudioRecorder;
