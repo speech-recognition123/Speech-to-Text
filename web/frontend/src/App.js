d90df89a217be34b7f7cf94bd201cd0ed2896230
Samrawit02 <hairaer02.gmail.com>
2022-06-09 10:43:23 +0300

    editing the app js

--- web/frontend/src/App.js
+++ web/frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { useState }  from 'react'
-import AudioUpload from './AudioUpload'
+import AudioUpload from './components/AudioUpload'
 import AudioFileUploader from "./components/AudioRecorder";
 import axios from 'axios';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
@@ -15,59 +15,7 @@ let audioContext = null;
 
 
 function App() {
-//   const startRecording = () => {
-//   let constraints = {
-//     audio: true,
-//     video: false
-// }
 
-// audioContext = new window.AudioContext();
-// console.log("sample rate: " + audioContext.sampleRate);
-
-// navigator.mediaDevices
-//     .getUserMedia(constraints)
-//     .then(function (stream) {
-//         console.log("initializing Recorder.js ...");
-
-//         gumStream = stream;
-
-//         let input = audioContext.createMediaStreamSource(stream);
-
-//         recorder = new window.Recorder(input, {
-//             numChannels: 1
-//         })
-
-//         recorder.record();
-//         console.log("Recording started");
-//     }).catch(function (err) {
-//         //enable the record button if getUserMedia() fails
-// });
-
-// }
-
-
-// const stopRecording = () => {
-// console.log("stopButton clicked");
-
-// recorder.stop(); //stop microphone access
-// gumStream.getAudioTracks()[0].stop();
-
-// recorder.exportWAV(onStop);
-// }
-
-// const onStop = (blob) => {
-// console.log("uploading...");
-
-// let data = new FormData();
-
-// data.append('text', "this is the transcription of the audio file");
-// data.append('wavfile', blob, "recording.wav");
-
-// const config = {
-//     headers: {'content-type': 'multipart/form-data'}
-// }
-// axios.post('http://localhost:8080/asr/', data, config);
-// }
 
 return (
 
