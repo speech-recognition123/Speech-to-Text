01b4f40b6a4464170e0570e852c5f3580a186018
Samrawit02 <hairaer02.gmail.com>
2022-06-09 11:19:34 +0300

    adding routes to app js

--- web/frontend/src/App.js
+++ web/frontend/src/App.js
@@ -3,7 +3,6 @@ import React  from 'react'
 import AudioUpload from './components/AudioUpload'
 import AudioFileUploader from "./components/AudioRecorder";
 import Home from "./components/Home";
-import axios from 'axios';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import "./App.css";
@@ -19,13 +18,15 @@ return (
   <BrowserRouter>
     <div className='App'>
     <Routes>
-      <Route path ='/' element = {<Home/>}/>
-
+   
+          <Route path ='/' element = {<Home/>}/>
+      
         </Routes>
+
       <Routes>
       <Route path ='/audio-uploader' element = {<AudioUpload/>}/>
-
         </Routes>
+        
         <Routes>
       <Route path ='/audio-recorder' element = {<AudioFileUploader/>}/>
 
--- web/frontend/src/components/Home.js
+++ web/frontend/src/components/Home.js
@@ -4,6 +4,7 @@ export default function componentName() {
   return (
     <>
     Home Page
+    
     </>
   );
 }
